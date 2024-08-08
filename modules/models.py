21ff5f9f485f70f120662518940dc26d25574eb3
Chekhov1337 <pavel.miridonov123@gmail.com>
2024-08-08 11:45:01 +0400

    Add ARIMA models

--- modules/models.py
+++ modules/models.py
@@ -1,12 +1,68 @@
 from statsmodels.tsa.holtwinters import ExponentialSmoothing
+import pmdarima as pm
+import statsmodels as sm
+import numpy as np
 
 
 def holt_winters(train, start=109, end=120, seasonal_periods=12):
-    predicts = list()
+    predictions = list()
 
-    for i in range(len(train)):
-        model = ExponentialSmoothing(train[i], seasonal=None, seasonal_periods=seasonal_periods).fit()
+    for ts in train:
+        model = ExponentialSmoothing(ts, seasonal=None, seasonal_periods=seasonal_periods).fit()
         predict = model.predict(strart=start, end=end)
-        predicts.append(predict)
+        predictions.append(predict)
 
-    return predicts
+    return predictions
+
+
+def auto_arima(train, season=False, m=1, num_predictions=12):
+    predictions = list()
+
+    for ts in train:
+        model = pm.auto_arima(ts, seasonal=season, stepwise=season, m=m, suppress_warnings=True,
+                              error_action="ignore", max_order=None, trace=True)
+
+        predictions.append(model.predict(num_predictions=num_predictions))
+
+    return predictions
+
+
+def ar_model(train, season=False, m=1, num_predictions=12):
+    predictions = []
+    for ts in train:
+        ar = pm.auto_arima(ts, seasonal=season, stepwise=season, m=m, suppress_warnings=True,
+                           error_action="ignore", max_order=None, trace=True, max_q=0, start_q=0, d=0)
+        predictions.append(ar.predict(n_periods=num_predictions))
+
+    return predictions
+
+
+def ma_model(train, season=False, m=1, num_predictions=12):
+    predictions = []
+    for ts in train:
+        ma = pm.auto_arima(ts, seasonal=season, stepwise=season, m=m, suppress_warnings=True,
+                           error_action="ignore", max_order=None, trace=True, max_p=0, start_p=0, d=0)
+        predictions.append(ma.predict(n_periods=num_predictions))
+
+    return predictions
+
+
+def arma_model(train, test, to_plot=False, season=False, m=1):
+    predictions = []
+    for ts in train:
+        arma = pm.auto_arima(train[i], seasonal=season, stepwise=season, m=m, suppress_warnings=True,
+                             error_action="ignore", max_order=None, trace=True, d=0)
+        predictions.append(arma.predict(n_periods=12))
+
+    return predictions
+
+# def trend_model(train, period=12, num_predictions=12):
+#     predictions = []
+#     for ts in train:
+#         decompose = sm.tsa.seasonal.seasonal_decompose(ts, period=period)
+#         trend = decompose.trend
+#         trend_new = np.ndarray(shape=120)
+#         for i in range(108):
+#             trend_new[i] = trend[i]
+#         for i in range(12):
+#             trend_new[108 + i] = None
