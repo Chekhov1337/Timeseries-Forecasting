348cdd20ad505ae30a72529295aeba0807e7ed8e
Chekhov1337 <pavel.miridonov123@gmail.com>
2024-08-14 13:32:52 +0400

    Stats table

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ from modules.stats import *
 values = read_tsf()
 x_train, x_test = split_data(values)
 
-arma = arma_model(x_train)
-plot(x_train, arma, x_test, 'ARMA')
+holt = holt_winters(x_train)
+metrics, avg = metrics(holt, x_test, True)
--- modules/stats.py
+++ modules/stats.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
 import os
 import numpy as np
+import pandas as pd
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_percentage_error, root_mean_squared_error
+from tabulate import tabulate
 
 
 def plot(original_values, predictions, test_values, label, start_n=1, save_to_file=False):
@@ -33,3 +36,67 @@ def plot(original_values, predictions, test_values, label, start_n=1, save_to_fi
             plt.savefig(os.path.join(results_dir, sample_file_name))
 
         plt.show()
+
+
+def metrics(predictions, test, print_tables=False):
+    metrics = {
+        'RMSE': [],
+        'R^2': [],
+        'MAPE': [],
+        'SMAPE': []
+    }
+
+    # Calculate metrics for each series
+    for test, pred in zip(test, predictions):
+        actual_values = test.to_numpy()
+        predicted_values = pred
+
+        # Ensure the predicted values and actual values have the same length
+        min_length = min(len(actual_values), len(predicted_values))
+        actual_values = actual_values[:min_length]
+        predicted_values = predicted_values[:min_length]
+
+        # Calculate RMSE
+        rmse = root_mean_squared_error(actual_values, predicted_values)
+        metrics['RMSE'].append(rmse)
+
+        # Calculate R^2
+        r2 = r2_score(actual_values, predicted_values)
+        metrics['R^2'].append(r2)
+
+        # Calculate MAPE
+        mape = mean_absolute_percentage_error(actual_values, predicted_values)
+        metrics['MAPE'].append(mape)
+
+        # Calculate SMAPE
+        smape = np.mean(
+            np.abs(predicted_values - actual_values) /
+            ((np.abs(predicted_values) + np.abs(actual_values)) / 2)
+        )
+        metrics['SMAPE'].append(smape)
+
+    mean_rmse = np.mean(metrics['RMSE'])
+    mean_r2 = np.mean(metrics['R^2'])
+    mean_mape = np.mean(metrics['MAPE'])
+    mean_smape = np.mean(metrics['SMAPE'])
+
+    # Create a new dictionary for the average metrics
+    average_metrics = {
+        'RMSE': [mean_rmse],
+        'R^2': [mean_r2],
+        'MAPE': [mean_mape],
+        'SMAPE': [mean_smape]
+    }
+
+    if print_tables:
+        print('Average Metrics:')
+        print(tabulate(average_metrics, headers=['RMSE', 'R^2', 'MAPE', 'SMAPE'], tablefmt="rounded_outline", numalign='left'))
+        print('Metrics for each timeseries:')
+        print(tabulate(metrics, headers=['RMSE', 'R^2', 'MAPE', 'SMAPE'], tablefmt="rounded_outline", numalign='left'))
+
+    # Create a DataFrame for the average metrics
+    average_metrics_df = pd.DataFrame(average_metrics)
+    # Create a DataFrame to display the individual metrics
+    metrics_df = pd.DataFrame(metrics)
+
+    return metrics_df, average_metrics_df
